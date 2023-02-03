5733470e9a1a70ee83ec5ba20b70a5398b8eb790
zydiz <ardatangkr@gmail.com>
2023-02-03 12:16:56 +0300

    add: readPost testing in place

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,104 @@
 #include <Arduino.h>
 
+#include <WiFi.h>
+
 #include "FS.h"
 #include "SD.h"
 #include "PSRamFS.h"
+
+#define BUFSIZE 1024
+
+
+const char* ssid       = "FREKANS";
+const char* password   = "Frekans1000hz";
+
+const char* httpHost    = "c12edaf5-27c4-441a-9bbf-8587e5c8471c.mock.pstmn.io";
+const char* httpPath   = "/post_test";
+const char* deviceId   = "123456";
+
+
+void readPost(fs::FS &fs, const char * path) {
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if(!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  size_t fLen = file.size();
+  char * fileBuffer = new char[BUFSIZE + 1];
+
+  WiFiClient client;
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(httpHost, 80)) {
+    Serial.println("Connection failed!");
+    return;
+  } else {
+    Serial.println("Connected to server!");
+    client.println();
+    client.println();
+    client.println(String("POST ") + httpPath + " HTTP/1.1");
+
+    // Make a HTTP request
+    client.println(String("Host: ") + httpHost);
+    client.println("Connection: close");
+    client.println("Content-Type: text/plain");
+    client.print("Content-Length: ");
+    client.println(fLen + 29);
+    client.println();
+
+    client.print(String("{\"DeviceId\":\"") + deviceId + "\",\"Data\":\"");
+
+    uint8_t skip = 0;
+    size_t written = 0;
+    while(file.available()){
+      size_t toRead = file.available();
+      if(toRead > BUFSIZE) {
+        toRead = BUFSIZE;
+      }
+      for(uint16_t i = 0; i < toRead; i++) {
+        if (skip == 0) {
+          fileBuffer[i] = file.read();
+          if (fileBuffer[i] == '\r') {
+            if (file.peek() == '\n') {
+              skip = 1;
+              fileBuffer[i] = '*';
+            }
+          }
+        } else {
+          skip = 0;
+          i -= 1;
+          file.read();
+        }
+      }
+      written = client.write(fileBuffer, toRead);
+      Serial.println(written);
+      // client.flush();
+      // Serial.println(toRead);
+      // Serial.println(fileBuffer);
+      delay(100);
+    }
+    client.print("\"}");
+    client.print("\r\n");
+
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      if (line == "\r") {
+        Serial.println("headers received");
+        break;
+      }
+    }
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+    }
+    Serial.println();
+  }
+  client.stop();
+  delete [] fileBuffer;
+  return;
+}
 
 
 uint8_t initSDcard(){
@@ -227,8 +323,12 @@ void setup(){
     return;
   }
 
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  WiFi.waitForConnectResult();
+
   Serial.println(FileName);
-  File file = PSRamFS.open(FileName, "r+"); // PSRamFS instead of SD
+  File file = PSRamFS.open(FileName, "r+");
   if(!file){
     Serial.println("File doesn't exist");
     Serial.println("Creating file...");
@@ -251,6 +351,9 @@ void setup(){
 
   Serial.print("\r\n\r\n---------------------------\r\n\r\n--------copied file--------\r\n\r\n---------------------------\r\n\r\n");
   readFile(SD, FileName.c_str());
+
+  Serial.print("\r\n\r\n---------------------------\r\n\r\n--------copied file--------\r\n\r\n---------------------------\r\n\r\n");
+  readPost(SD, FileName.c_str());
 }
 
 void loop(){
